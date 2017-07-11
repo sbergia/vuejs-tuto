b09eeb06cccb3a399ffac6ba2c3499ebdad10e77
bergia <Ippon001>
2017-07-11 12:45:19 +0200

    chap9 : vue-router tutorial ; new Menu component to illustrate router-link

---
+++ src/components/chap9/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="menu">
+    <ul>
+      <li><router-link :to="'/vuejs-tuto/chap1'">Chap1</router-link></li>
+      <li><router-link :to="'/vuejs-tuto/chap2'">Chap2</router-link></li>
+      <!--li>
+        <router-link :to="'/vuejs-tuto/chap9/123123123?firstname=Maggie&lastname=Simpson'">Chap9</router-link>
+      </li-->
+      <li><router-link :to="{ name: 'pagechap9', params: {id: 123123123}, query: {firstname: 'Maggie', lastname: 'Simpson'}}">Chap9</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+</script>
